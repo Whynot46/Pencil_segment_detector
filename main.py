de0da890a23ea46ce1c994abde57e3e811973434
Aleksey_Pahalev <83154713+Whynot46@users.noreply.github.com>
2023-07-04 11:16:20 +0400

    Update main.py

--- main.py
+++ main.py
@@ -12,20 +12,19 @@ def open_file(file_path):
 def show_frame(capture):
   while(capture.isOpened()):
     ret, frame = capture.read()
-    
     if ret == True:
       resized_frame = cv2.resize(frame, (1000, 600))
-
       blur_frame=cv2.medianBlur(resized_frame, 15)
       thresh_frame = cv2.inRange(blur_frame, (100, 50, 0), (180, 180 , 100))
 
-      lines=cv2.HoughLinesP(thresh_frame, 2, np.pi/180, 175, None, 55, 15)
+      #cv2.HoughLinesP(image, rho, theta, threshold, lines, minLineLength, maxLineGap) 
+      lines=cv2.HoughLinesP(thresh_frame, 2, np.pi/180, 175, None, 10, 20)
 
       try:
         for line in lines:
             line = line[0]
-            cv2.line(resized_frame, (line[0],line[1]),(line[2],line[3]),(0,0,255),3)  
-      except: print('Lines not found')
+            cv2.line(resized_frame, (line[0],line[1]), (line[2],line[3]), (0,0,255), 3)  
+      except: pass
 
       cv2.imshow('Original frame', resized_frame)
       cv2.imshow('Thresh frame', thresh_frame)
